b95496a9f017a88e12ae7f755019af4a2f759b7f
shubham <shubhamsinghmor2312@gmail.com>
2024-10-31 13:56:20 +0530

    added profile route and profilepicture route

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -25,10 +25,12 @@ export const register=async(req,res)=>{
             password:hashedPassword,
         })
         await newUser.save();
-
+        console.log(newUser)
+        
         const profile=new Profile({
             userId:newUser._id,
         })
+        await profile.save();
         return res.json({message:"User Created"})
     }catch(err){
         return res.status(500).json({message:err.message})
@@ -42,7 +44,7 @@ export const login=async(req,res)=>{
         if(!email || !password){
             return res.status(400).json({message:"All fields are required"})
         }
-        const user=User.findOne({
+        const user=await User.findOne({
             email
         })
         if(!user){
@@ -54,9 +56,67 @@ export const login=async(req,res)=>{
         }
         const token=crypto.randomBytes(32).toString("hex");
         await User.updateOne({_id:user._id},{token})
+        console.log("user logged in",user.username)
         return res.json({token})
     }catch(err){
         return res.status(400).json({message:err.message})
     }
 
+}
+export const uploadProfilePicture=async(req,res)=>{
+
+    const {token}=req.body;
+    try{
+
+        const user=await User.findOne({token:token})
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+        user.profilePicture=req.file.filename;
+        await user.save();
+        
+        return res.json({message:"Profile Picture Updated"})
+
+    }catch(err){
+        return res.status(400).json({message:err.message})
+    }
+}
+export const updateUserProfile=async(req,res)=>{
+     try{
+        const {token,...newUserData}=req.body;
+        const user=await User.findOne({token:token});
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const {username,email}=newUserData;
+        const existingUser=await User.findOne({$or:[{username},{email}]})
+
+        if(existingUser){
+
+            if(existingUser || String(existingUser._id) !==String(user._id)){
+                return res.status(400).json({message:"User already exists"})
+            }
+        }
+        Object.assign(user,newUserData);
+        await user.save();
+     }catch(err){
+        res.status(500).json({message:err.message})
+     }
+
+}
+export const getUserAndProfile =async(req,res)=>{
+    const {token}=req.body;
+    try{
+        const user=await User.findOne({token:token});
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
+
+
+    }catch(err){p
+        return res.josn({message:err.message})
+    }
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,18 @@
 import {Router} from 'express'
-// import { activecheck } from '../controllers/post.controllers.js'
-import { login, register } from '../controllers/user.controllers.js';
+import multer from 'multer'
+
+import { login, register,uploadProfilePicture,updateUserProfile } from '../controllers/user.controllers.js';
 
 const router=Router();
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'uploads/')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+const upload =multer({storage:storage})
 
 router
 .route('/register')
@@ -11,4 +21,19 @@ router
 router
 .route('/login')
 .post(login)
+
+router 
+.route('/update_profile_picture')
+.post(upload.single('profile_picture'),uploadProfilePicture)
+
+
+router
+.route("/user_update")
+.post(updateUserProfile)
+
+router.route('/get_user_and_profile')
+
+
+
 export default router
+
