9c16ba7d96817b024fa39aae0256ae5cce386e3e
Shubh1127 <shubhamsinghmor2312@gmail.com>
2025-08-01 16:23:52 +0530

    started working on the dashboard

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 const Dashboard = () => {
+  const [visible,setVisible]=useState(true)
     const router = useRouter();
     useEffect(()=>{
         if(!localStorage.getItem("token")){
+          setVisible(false)
+          setTimeout(() => {
             router.push('/login')
+          }, 500)
         }
-    })
+    }, [router])
   return (
-    <div>dashboard</div>
+    <div className={`transition-opacity duration-500 ${visible? 'opacity-100': 'opacity-0'}`}>dashboard</div>
   )
 }
 
